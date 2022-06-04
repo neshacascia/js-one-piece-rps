cdf78f0636bae2e0f9767315ba7f6a4e03eb8f77
Nesha Mervin <nesha.mervin@gmail.com>
2022-06-03 22:16:31 -0400

    Added opponent selector and hover states

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ document.querySelector('#luffy').addEventListener('click', luffyBattle);
 document.querySelector('#zoro').addEventListener('click', zoroBattle);
 document.querySelector('#ace').addEventListener('click', aceBattle);
 document.querySelector('#shanks').addEventListener('click', shanksBattle);
+document.querySelector('#big-mom').addEventListener('click', bigMomBattle);
+document.querySelector('#kaido').addEventListener('click', kaidoBattle);
 
 const luffy = document.querySelector('.luffy-battle');
 const zoro = document.querySelector('.zoro-battle');
 const ace = document.querySelector('.ace-battle');
 const shanks = document.querySelector('.shanks-battle');
+const bigMom = document.querySelector('.big-mom-battle');
+const kaido = document.querySelector('.kaido-battle');
 
 function luffyBattle() {
     luffy.classList.toggle('hidden');
@@ -36,11 +40,12 @@ function shanksBattle() {
     ace.classList.add('hidden');
 }
 
-// Background Selector
-document.querySelector('#bg-1').addEventListener('click', loadBG1);
+function bigMomBattle() {
+    bigMom.classList.toggle('hidden');
+    kaido.classList.add('hidden');
+}
 
-const cave = document.querySelector('#bg-1');
-
-function loadBG1() {
-    document.querySelector('.game-screen').style.backgroundImage = cave;
+function kaidoBattle() {
+    kaido.classList.toggle('hidden');
+    bigMom.classList.add('hidden');
 }
