a341947c237bf19eedb5a9ce1d922399ad5dd670
Nesha Mervin <nesha.mervin@gmail.com>
2023-06-01 11:17:29 -0400

    deleted old code logic

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// //REFACTORED VERSION:
 const playerSprites = Array.from(document.querySelectorAll('.sprite-fighter'));
 
 const playerSpritesBattle = Array.from(
@@ -20,45 +19,6 @@ function playerBattle(e) {
     }
   });
 }
-
-//PREVIOUS VERSION:
-// document.querySelector('#luffy').addEventListener('click', luffyBattle);
-// document.querySelector('#zoro').addEventListener('click', zoroBattle);
-// document.querySelector('#ace').addEventListener('click', aceBattle);
-// document.querySelector('#shanks').addEventListener('click', shanksBattle);
-
-// const luffy = document.querySelector('.luffy-battle');
-// const zoro = document.querySelector('.zoro-battle');
-// const ace = document.querySelector('.ace-battle');
-// const shanks = document.querySelector('.shanks-battle');
-
-// function luffyBattle() {
-//   luffy.classList.toggle('hidden');
-//   zoro.classList.add('hidden');
-//   ace.classList.add('hidden');
-//   shanks.classList.add('hidden');
-// }
-
-// function zoroBattle() {
-//   zoro.classList.toggle('hidden');
-//   luffy.classList.add('hidden');
-//   ace.classList.add('hidden');
-//   shanks.classList.add('hidden');
-// }
-
-// function aceBattle() {
-//   ace.classList.toggle('hidden');
-//   luffy.classList.add('hidden');
-//   zoro.classList.add('hidden');
-//   shanks.classList.add('hidden');
-// }
-
-// function shanksBattle() {
-//   shanks.classList.toggle('hidden');
-//   luffy.classList.add('hidden');
-//   zoro.classList.add('hidden');
-//   ace.classList.add('hidden');
-// }
 
 //ROCK PAPER SCISSORS LOGIC:
 const userChoiceDisplay = document.querySelector('#user-choice');
